2efc071d18dba705993c53876db234a09850f5fb
masfreddy93 <masieronicola@gmail.com>
2022-11-16 09:40:38 +0100

    first commit

---
+++ src/org/booleancarrers/java/Snack1.java
@@ -0,0 +1,24 @@
+package org.booleancarrers.java;
+
+import java.util.Scanner;
+
+public class Snack1 {
+
+	
+	public static void main(String[] args) {
+		
+//		Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Inserire numero: ");
+		int number = sc.nextInt();
+		if(number % 2 == 0) {
+			System.out.println(number);
+		}else {
+			System.out.println(number + 1);
+		}
+		
+		sc.close();
+	}
+}
---
+++ src/org/booleancarrers/java/Snack2.java
@@ -0,0 +1,12 @@
+package org.booleancarrers.java;
+
+public class Snack2 {
+
+	public static void main(String[] args) {
+		
+		/* Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, 
+		e da queste vuole mostrare a video una falsa lista di invitati con nome e cognome. */
+		
+		
+	}
+}
