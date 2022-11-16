2313807f9dda84428897b2ce79090831e92d036b
masfreddy93 <masieronicola@gmail.com>
2022-11-16 16:06:47 +0100

    snack 8

--- src/org/booleancarrers/java/Snack8.java
+++ src/org/booleancarrers/java/Snack8.java
@@ -1,9 +1,47 @@
 package org.booleancarrers.java;
 
+import java.util.Random;
+
 public class Snack8 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("prova prova");
+		/* Creare un array di 10 elementi interi random compresi tra -50 e +50 
+		 * (utilizzare l'oggetto random con seed 128 new Random(128))
+
+				// Risultato atteso
+				new int[] { 39, -32, -48, 25, 17, -27, 8, -13, -30, 40 }
+				trovare il valore assoluto piu' grande e stampare il valore corrispondente
+				// Risultato atteso
+				(int) -48
+				trovare il valore assoluto piu' piccolo e stampare il valore corrispondente
+				// Risultato atteso
+				(int) 8
+				calcolare la sommatoria dei valori multipli di 5
+				// Risultato atteso
+				(int) 35  */
+		
+		
+		Random r = new Random(128);
+		int[] arr = new int[10];
+		
+		int maxAbs = arr[0];
+		int minAbs = arr[0];
+		int sumMultipleOfFive = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			
+			int value = r.nextInt(100);
+			arr[i] = value - 50;
+			System.out.print(arr[i] + " ");
+			
+			
+			
+		}
+		
+		System.out.println("\n\nValore assoluto più grande: " + maxAbs);
+		System.out.println("Valore assoluto più piccolo: " + minAbs);
+		System.out.println("Sommatoria multipli di 5: " + sumMultipleOfFive);
+		
 	}
 }
