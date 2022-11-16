5867a0e3f75f8079e18dceed9783826fe6da6c0d
masfreddy93 <masieronicola@gmail.com>
2022-11-16 09:59:19 +0100

    second commit

--- src/org/booleancarrers/java/Snack2.java
+++ src/org/booleancarrers/java/Snack2.java
@@ -1,6 +1,5 @@
 package org.booleancarrers.java;
 
-import java.util.Random;
 
 public class Snack2 {
 
@@ -9,12 +8,9 @@ public class Snack2 {
 		/* Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, 
 		e da queste vuole mostrare a video una falsa lista di invitati con nome e cognome. */
 		
-		Random r = new Random();
-		
 		
 		String[] nomi = {"Mario", "Giuseppe", "Anna", "Cristina", "Luca"};
 		String [] cognomi = {"Verdi", "Rossi", "Bianchi", "Lopez", "Busi"};
-		
 		
 		System.out.println("Lista invitati: \n");
 		for(int i = 0; i < nomi.length; i++) {
