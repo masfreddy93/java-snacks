283b035217e2e1cbd22b59ad9177a258864acae6
masfreddy93 <masieronicola@gmail.com>
2022-11-16 10:03:22 +0100

    refactoring

--- src/org/booleancarrers/java/Snack1.java
+++ src/org/booleancarrers/java/Snack1.java
@@ -9,17 +9,19 @@ public class Snack1 {
 		
 //		Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 		
+		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Inserire numero: ");
 		int number = sc.nextInt();
 		
+		sc.close();
+		
+		
 		if(number % 2 == 0) {
 			System.out.println(number);
-		}else {
+		} else {
 			System.out.println(number + 1);
 		}
-		
-		sc.close();
 	}
 }
